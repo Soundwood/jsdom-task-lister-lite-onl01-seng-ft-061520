af5ee313f1dd4243c6d8d570b57b9f6d63e8d89d
Soundwood <j.benhogan@gmail.com>
2020-09-21 05:51:28 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const form = document.getElementById("create-task-form")
+let list = document.getElementById("tasks")
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  let text = document.getElementById("new-task-description")
+  let element = document.createElement('li')
+  element.innerText = text.value
+  list.appendChild(element)
+  event.target.reset()
+})
